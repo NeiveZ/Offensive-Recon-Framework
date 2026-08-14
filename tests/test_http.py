7d22d513141f9440373ea3af09aad6d764e3c819
NeiveZ <113144519+NeiveZ@users.noreply.github.com>
2026-08-14 07:46:03 -0300

    Delete tests directory

--- tests/test_http.py
+++
@@ -1,6 +0,0 @@
-from modules.http_probe import HTTPProbe
-
-
-def test_http_candidates():
-    probe = HTTPProbe()
-    assert probe._candidates("example.com")[0].startswith("https://")
--- tests/test_tls.py
+++
@@ -1,6 +0,0 @@
-from modules.tls_probe import TLSProbe
-
-
-def test_tls_options():
-    probe = TLSProbe()
-    assert probe.get_option("VERIFY") == "true"
